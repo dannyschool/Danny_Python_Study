a8ea10b61d6596bfe55a3496e6ee5265cad9be19
dannyschool <dannyschool@hotmail.com>
2015-11-29 23:05:18 +0800

    ex4
    Variable Test

---
+++ ex5.py
@@ -0,0 +1,9 @@
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74 #inches
+my_weight = 180 # lbs
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print ("Let's talk about %s." %my_name)
+print ("He's %s inches tall."%my_age)
